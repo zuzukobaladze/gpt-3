2e3f34c05674fae20fa5ae7da07c4a8ab8de4c2d
Zurabi Kobaladze <kobaladze.zurabi@kiu.edu.ge>
2022-08-28 16:55:40 +0400

    first commit

---
+++ build/precache-manifest.06df59ffdbc17ffb6a31364e2adcf0d3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "094a72db593a7e82dbfc",
+    "url": "/static/css/main.2c8b80fc.chunk.css"
+  },
+  {
+    "revision": "094a72db593a7e82dbfc",
+    "url": "/static/js/main.094a72db.chunk.js"
+  },
+  {
+    "revision": "b80769ea712c754fd21e",
+    "url": "/static/js/1.b80769ea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "7c8dcf9ee18bf446175f85e1c4e32fea",
+    "url": "/index.html"
+  }
+];
